aa06073663bf8b48f3367da6fe3bbe731a50a4a1
Eunji Song <ijaysong0811@gmail.com>
2020-01-28 23:16:36 +0900

    요청매핑 : 3. 메서드 속성(GET/POST/DELETE/PUT/PATCH), 4.요청 헤더(X-HTTP-Method-Override)

--- src/main/java/org/hdcd/controller/BoardController.java
+++ src/main/java/org/hdcd/controller/BoardController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 	
+	// GET방식
 	@RequestMapping(value="", method=RequestMethod.GET)
 	public ResponseEntity<List<Board>> list() {
 		logger.info("list");
@@ -47,15 +48,16 @@ public class BoardController {
 		return entity;
 	}
 	
+	// POST방식
 	@RequestMapping(value = "", method = RequestMethod.POST)
 	public ResponseEntity<String> register(Board board) {
 		// Q.파라미터의 @RequestBody를 지우고 나서 $.post의 415에러(Unsupported Media Type)가 해결되었다. 왜??
-		// A.@RequestBody 어노테이션은 POST 방식으로 전송된 HTTP 요청 데이터를 String 타입의 body 파라미터로 전달한다.
 		logger.info("register");
 		
 		return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
 	}
 	
+	// GET방식
 	@RequestMapping(value = "/{boardNo}", method = RequestMethod.GET)
 	public ResponseEntity<Board> read(@PathVariable("boardNo") int boardNo) {
 		logger.info("read");
@@ -73,6 +75,7 @@ public class BoardController {
 		return entity;
 	}
 	
+	// DELETE방식
 	@RequestMapping(value="/{boardNo}", method=RequestMethod.DELETE)
 	public ResponseEntity<String> remove(@PathVariable("boardNo") int boardNo) {
 		logger.info("remove");
@@ -81,8 +84,11 @@ public class BoardController {
 		return entity;
 	}
 	
+	// PUT방식
 	@RequestMapping(value="/{boardNo}", method=RequestMethod.PUT)
-	public ResponseEntity<String> remove(@PathVariable("boardNo") int boardNo, @RequestBody Board board) {
+	public ResponseEntity<String> modifyByPut(@PathVariable("boardNo") int boardNo, @RequestBody Board board) {
+		// @RequestBody:컨트롤러로 전송된 JSON이 해당 데이터 타입으로 받을 수 있도록 변환해준다.
+		// @ResponseBody:컨트롤러에서 반환되는 값을 JSON으로 변환하여 보낸다. 
 		logger.info("modify");
 		ResponseEntity<String> entity = new ResponseEntity<>("SUCCESS", HttpStatus.OK);
 		
@@ -99,5 +105,23 @@ public class BoardController {
 	public String readBoard2(@PathVariable("no") int boardNo) {
 		logger.info("read2 boardNo:" + boardNo);
 		return "READ2 boardNo:"+boardNo;
+	}
+	
+	// Patch방식
+	@RequestMapping(value="/{boardNo}", method=RequestMethod.PATCH)
+	public ResponseEntity<String> modifyByPatch(@PathVariable("boardNo") int boardNo, @RequestBody Board board) {
+		logger.info("modifyByPatch");
+		
+		ResponseEntity<String> entity = new ResponseEntity<>("SUCCESS", HttpStatus.OK);
+		return entity;
+	}
+	
+	// Header 속성을 사용하여 요청을 매핑한다 
+	@RequestMapping(value="/{boardNo}", method=RequestMethod.PUT, headers="X-HTTP-Method-Override=PUT")
+	public ResponseEntity<String> modifyByHeader(@PathVariable("boardNo") int boardNo, @RequestBody Board board){
+		logger.info("modifyByHeader");
+		
+		ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+		return entity;
 	}
 }
