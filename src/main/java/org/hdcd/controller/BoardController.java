ddcf13fe4ef7236d30564e510e723f7174f329ff
Eunji Song <ijaysong0811@gmail.com>
2020-01-29 22:56:02 +0900

    요청매핑 : 5.Content-Type헤더 값

--- src/main/java/org/hdcd/controller/BoardController.java
+++ src/main/java/org/hdcd/controller/BoardController.java
@@ -85,7 +85,7 @@ public class BoardController {
 	}
 	
 	// PUT방식
-	@RequestMapping(value="/{boardNo}", method=RequestMethod.PUT)
+	@RequestMapping(value="/{boardNo}", method=RequestMethod.PUT, consumes="application/json")
 	public ResponseEntity<String> modifyByPut(@PathVariable("boardNo") int boardNo, @RequestBody Board board) {
 		// @RequestBody:컨트롤러로 전송된 JSON이 해당 데이터 타입으로 받을 수 있도록 변환해준다.
 		// @ResponseBody:컨트롤러에서 반환되는 값을 JSON으로 변환하여 보낸다. 
@@ -120,6 +120,17 @@ public class BoardController {
 	@RequestMapping(value="/{boardNo}", method=RequestMethod.PUT, headers="X-HTTP-Method-Override=PUT")
 	public ResponseEntity<String> modifyByHeader(@PathVariable("boardNo") int boardNo, @RequestBody Board board){
 		logger.info("modifyByHeader");
+		
+		ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+		return entity;
+	}
+	
+	// consumes 속성을 사용하여 미디어 타입을 지정한다
+	// consumes 속성을 지정하지 않으면, 기본값인 application/json"이 지정된다
+	@RequestMapping(value="/{boardNo}", method=RequestMethod.PUT, consumes="application/xml")
+	public ResponseEntity<String> modifyByJson(@PathVariable("boardNo") int boardNo, @RequestBody Board board){
+		logger.info("modifyByXml boardNo: " + boardNo);
+		logger.info("modifyByXml board: " + board);
 		
 		ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
 		return entity;
