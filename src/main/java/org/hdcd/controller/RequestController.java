706e18677d683ea71184d2886e93ed55449c7049
Eunji Song <ijaysong0811@gmail.com>
2020-02-03 22:59:01 +0900

    컨트롤러 요청 처리 : 자바 빈즈

--- src/main/java/org/hdcd/controller/RequestController.java
+++ src/main/java/org/hdcd/controller/RequestController.java
@@ -1,10 +1,16 @@
 package org.hdcd.controller;
 
+import java.util.List;
+
+import org.hdcd.domain.Address;
+import org.hdcd.domain.Card;
+import org.hdcd.domain.Member;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -51,6 +57,92 @@ public class RequestController {
 		logger.info("request04");
 		logger.info("userId= " + userId);
 		logger.info("password= " + password);
+		
+		ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+		return entity;
+	}
+	
+	// 객체 타입의 JSON 요청 데이터를 @RequestBody 애너테이션을 지정하여 자바빈즈 매개변수로 처리한다
+	@RequestMapping(value="/request05", method=RequestMethod.POST)
+	public ResponseEntity<String> request05(@RequestBody Member member) {
+		logger.info("request05");
+		
+		logger.info("userId: " + member.getUserId());
+		logger.info("password: " + member.getPassword());
+		
+		ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+		return entity;
+	}
+	
+	// 객체 타입의 JSON 요청 데이터를 @PathVariable 애너테이션을 지정한 문자열 매개변수와 @RequestBody 애너테이션을 지정한 자바빈즈 매개변수로 지정한다
+	@RequestMapping(value="/request06/{userId}", method=RequestMethod.POST)
+	public ResponseEntity<String> request06(@PathVariable("userId") String userId, @RequestBody Member member) {
+		logger.info("request06");
+		
+		logger.info("userId: " + userId);
+		logger.info("member.getUserId(): " + member.getUserId());
+		logger.info("member.getPassword(): " + member.getPassword());
+		
+		ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+		return entity;
+	}
+	
+	// 객체 배열 타입의 JSON 요청 데이터를 자바빈즈요소를 가진 리스트 컬렉션 매개변수에 @RequestBody 애너테이션을 지정하여 처리한다
+	@RequestMapping(value="/request07", method=RequestMethod.POST)
+	public ResponseEntity<String> request07(@RequestBody List<Member> memberList) {
+		logger.info("register07");
+		
+		for(Member member : memberList) {
+			logger.info("userId: " + member.getUserId());
+			logger.info("password: " + member.getPassword());
+		}
+		
+		ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+		return entity;
+	}
+	
+	// 중첩된 객체 타입의 JSON 요청 데이터를 @RequestBody 애너테이션을 지정하여 중첩된 자바빈즈 매개변수로 처리한다
+	@RequestMapping(value="/request08", method=RequestMethod.POST)
+	public ResponseEntity<String> request08(@RequestBody Member member) {
+		logger.info("register08");
+		
+		logger.info("userId: " + member.getUserId());
+		logger.info("password: " + member.getPassword());
+		
+		Address address= member.getAddress();
+		
+		if(address != null) {
+			logger.info("address.getPostCode() = " + address.getPostCode());
+			logger.info("address.getLocation() = " + address.getLocation());
+		} else {
+			logger.info("address == null");
+		}
+		
+		ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+		return entity;
+	}
+	
+	// 중첩된 객체 배열 타입의 JSON 요청 데이터를 @RequestBody 애너테이션을 지정하여 중첩된 컬렉션 자바빈즈 매개변수로 처리한다
+	@RequestMapping(value="/request09", method=RequestMethod.POST)
+	public ResponseEntity<String> request09(@RequestBody Member member) {
+		logger.info("register09");
+		
+		logger.info("userId: " + member.getUserId());
+		logger.info("password: " + member.getPassword());
+		
+		List<Card> cardList= member.getCardList();
+		
+		if(cardList != null) {
+			logger.info("cardList.size() = " + cardList.size());
+			
+			for(int i = 0; i < cardList.size(); i++) {
+				Card card = cardList.get(i);
+				logger.info("card.getNo() = " + card.getNo());
+				logger.info("card.getValidMonth() = " + card.getValidMonth());
+			}
+		} else {
+			logger.info("cardList == null");
+		}
 		
 		ResponseEntity<String> entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
 		return entity;
