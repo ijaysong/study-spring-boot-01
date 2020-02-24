8aee6834cac75cec5192829ed324b62394f29fb6
Eunji Song <ijaysong0811@gmail.com>
2020-02-24 21:32:16 +0900

    마이바티스 : 일대다 관계 테이블 매핑 1

--- src/main/java/org/hdcd/mapper/MemberMapper.java
+++ src/main/java/org/hdcd/mapper/MemberMapper.java
@@ -1,5 +1,7 @@
 package org.hdcd.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.hdcd.domain.Member;
 import org.hdcd.domain.MemberAuth;
@@ -7,5 +9,10 @@ import org.hdcd.domain.MemberAuth;
 @Mapper
 public interface MemberMapper {
 	public void createMember(Member member) throws Exception;
+	public Member readMember(int userNo) throws Exception;
+	public void updateMember(Member member) throws Exception;
+	public void deleteMember(int userNo) throws Exception;
+	public List<Member> listMember() throws Exception;
 	public void createAuth(MemberAuth memberAuth) throws Exception;
+	public void deleteAuth(int userNo) throws Exception;
 }
