397132064213d77e08d74650f0a4ff33595fc182
Eunji Song <ijaysong0811@gmail.com>
2020-03-09 20:34:35 +0900

    파일 업로드 : 이미지 업로드 3

--- src/main/java/org/hdcd/controller/ItemController.java
+++ src/main/java/org/hdcd/controller/ItemController.java
@@ -1,17 +1,25 @@
 package org.hdcd.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.URI;
+import java.util.List;
 import java.util.UUID;
 
+import org.apache.commons.io.IOUtils;
 import org.hdcd.domain.Item;
 import org.hdcd.service.ItemService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -85,5 +93,118 @@ public class ItemController {
 		FileCopyUtils.copy(fileData, target);
 		
 		return createdFileName;
+	}
+	
+	@RequestMapping("/display")
+	public ResponseEntity<byte[]> displayFile(int itemId) throws Exception {
+		InputStream in = null;
+		ResponseEntity<byte[]> entity = null;
+		
+		String fileName = itemService.getPicture(itemId);
+		
+		logger.info("FILE NAME: " + fileName);
+		
+		try {
+			String formatName = fileName.substring(fileName.lastIndexOf(".") + 1);
+			MediaType mType = getMediaType(formatName);
+			
+			HttpHeaders headers = new HttpHeaders();
+			
+			in = new FileInputStream(uploadPath + File.separator + fileName);
+			
+			if(mType != null) {
+				headers.setContentType(mType);
+			}
+			
+			entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
+		} finally {
+			in.close();
+		}
+		return entity;
+	}
+	
+	private MediaType getMediaType(String formatName) {
+		if(formatName != null) {
+			if(formatName.equals("JPG")) {
+				return MediaType.IMAGE_JPEG;
+			}
+			
+			if(formatName.equals("GIF")) {
+				return MediaType.IMAGE_GIF;
+			}
+			
+			if(formatName.equals("PNG")) {
+				return MediaType.IMAGE_PNG;
+			}
+		}
+		return null;
+	}
+	
+	@RequestMapping(value="", method=RequestMethod.GET)
+	public ResponseEntity<List<Item>> list() throws Exception {
+		logger.info("list");
+		List<Item> itemList = this.itemService.list();
+		
+		return new ResponseEntity<>(itemList, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/{itemId}", method=RequestMethod.GET)
+	public ResponseEntity<Item> read(@PathVariable("itemId") int itemId) throws Exception {
+		logger.info("read");
+		
+		Item item = this.itemService.read(itemId);
+		
+		return new ResponseEntity<>(item, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/{itemId}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> remove(@PathVariable("itemId") int itemId) throws Exception {
+		logger.info("remove");
+		
+		this.itemService.remove(itemId);
+		
+		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+	}
+	
+	@RequestMapping(value="", method=RequestMethod.PUT)
+	public ResponseEntity<Void> modify(@RequestPart("item") String itemString, @RequestPart("file") MultipartFile picture) throws Exception {
+		logger.info("itemString: " + itemString);
+		
+		Item item = new ObjectMapper().readValue(itemString, Item.class);
+		
+		String itemName = item.getItemName();
+		String description = item.getDescription();
+		
+		if(itemName != null) {
+			logger.info("item.getItemName(): " + itemName);
+			
+			item.setItemName(itemName);
+		}
+		
+		if(description != null) {
+			logger.info("item.getDescription()" + description);
+			
+			item.setDescription(description);
+		}
+		
+		item.setPicture(picture);
+		
+		MultipartFile file = item.getPicture();
+		
+		logger.info("originalName: " + file.getOriginalFilename());
+		logger.info("size: " + file.getSize());
+		logger.info("contentType: " + file.getContentType());
+		
+		String createdFileName = uploadFile(file.getOriginalFilename(), file.getBytes());
+		
+		item.setPictureUrl(createdFileName);
+		
+		this.itemService.modify(item);
+		
+		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}
 }
