2cef46747558b83dd1b52d8be70f2ba97f8b8ca7
Eunji Song <ijaysong0811@gmail.com>
2020-03-14 19:32:39 +0900

    파일 업로드 : 비동기식 파일 업로드 1

--- src/main/java/org/hdcd/mapper/ItemMapper.java
+++ src/main/java/org/hdcd/mapper/ItemMapper.java
@@ -3,6 +3,7 @@ package org.hdcd.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.hdcd.domain.Item;
 
 @Mapper
@@ -10,29 +11,30 @@ public interface ItemMapper {
 	
 	// 이미지 업로드
 	public void create(Item item) throws Exception;
-	
 	public Item read(int itemId) throws Exception;
-	
 	public void update(Item item) throws Exception;
-	
 	public void delete(int itemId) throws Exception;
-	
 	public List<Item> list() throws Exception;
-	
 	public String getPicture(int itemId) throws Exception;
 	
 	// 여러 개의 이미지 업로드
 	public void createMultiple(Item item) throws Exception;
-	
 	public Item readMultiple(int itemId) throws Exception;
-	
 	public void updateMultiple(Item item) throws Exception;
-	
 	public void deleteMultiple(int itemId) throws Exception;
-	
 	public List<Item> listMultiple() throws Exception;
-	
 	public String getPictureMultiple1(int itemId) throws Exception;
-	
 	public String getPictureMultiple2(int itemId) throws Exception;
+	
+	// 비동기 방식 이미지 업로드
+	public void createAsync(Item item) throws Exception;
+	public Item readAsync(int itemId) throws Exception;
+	public void updateAsync(Item item) throws Exception;
+	public void deleteAsync(int itemId) throws Exception;
+	public List<Item> listAsync() throws Exception;
+	public String getPictureAsync() throws Exception;
+	public void addAttachAsync() throws Exception;
+	public List<String> getAttachAsync(int itemId) throws Exception;
+	public void deleteAttachAsync(int itemId) throws Exception;
+	public void replaceAttachAsync(@Param("fullName") String fullName, @Param("itemId") int itemId) throws Exception;
 }
