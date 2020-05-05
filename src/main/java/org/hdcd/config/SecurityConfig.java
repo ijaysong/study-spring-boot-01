d86350f5b5056f7f774865019a09b68d709eff1c
Eunji Song <ijaysong0811@gmail.com>
2020-05-05 16:44:34 +0900

    스프링 시큐리티 : 로그인 처리

--- src/main/java/org/hdcd/config/SecurityConfig.java
+++ src/main/java/org/hdcd/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.hdcd.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -25,4 +26,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		// 폼 기반 인증 기능을 사용한다
 		http.formLogin();
 	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		// 지정된 아이디와 패스워드로 로그인이 가능하도록 설정한다
+		// 스프링 시큐리티 5버전부터는 패스워드 암호화 처리기를 반드시 이용하도록 변경이 되었다
+		// 암호화 처리기를 사용하지 않도록 "{noop}" 문자열을 비밀번호 앞에 사용한다
+		auth.inMemoryAuthentication().withUser("member").password("{noop}1234").roles("MEMBER");
+		auth.inMemoryAuthentication().withUser("admin").password("{noop}1234").roles("ADMIN");
+	}
+	
+	
 }
